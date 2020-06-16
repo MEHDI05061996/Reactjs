9ec4734fd9b0262580c140c815d216398354153c
Mehdi <65984668+MEHDI05061996@users.noreply.github.com>
2020-06-16 20:48:31 +0200

    Update Forms.js

--- component/Forms.js
+++ component/Forms.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react'
 
  class Forms extends Component {
     state={
-    value : "Deafult"
+    value : "Deafult",
+    value1:"2"
     }
     setvalue=(event)=>{
         this.setState({
             value:event.target.value
         });
     }
+    setselectvalue=(event)=>{
+        this.setState({
+            value1:event.target.value
+        });
+    }
     render() {
         return (
             <div>
                 <input value={this.state.value} onChange={this.setvalue}/>
-          <hr></hr>
+                <hr></hr>
                 <textarea value={this.state.value} onChange={this.setvalue}></textarea>
+                <hr></hr>
+                <select value={this.state.value1} onChange={this.setselectvalue}>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">three</option>
+                </select>
             </div>
         )
     }
