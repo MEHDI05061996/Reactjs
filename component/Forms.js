6c2ebc7084fefb29f3b0054f1b291f8c35d0123b
Mehdi <65984668+MEHDI05061996@users.noreply.github.com>
2020-06-17 00:41:47 +0200

    Update Forms.js

--- component/Forms.js
+++ component/Forms.js
@@ -3,16 +3,32 @@ import React, { Component } from 'react'
  class Forms extends Component {
     state={
     value : "Deafult",
-    value1:"2"
+    valuetextarea : "textarea",
+    value1:"2",
+    checked:true
     }
+    // Forms - Controlled components
     setvalue=(event)=>{
         this.setState({
             value:event.target.value
         });
     }
+    //Forms - Textarea
+    settextareavalue=(event)=>{
+        this.setState({
+            valuetextarea: event.target.value
+        });
+    }
+    //Forms - Select
     setselectvalue=(event)=>{
         this.setState({
             value1:event.target.value
+        });
+    }
+    //Forms - Checkbox
+    setchecked=(event)=>{
+        this.setState({
+            checked:event.target.checked
         });
     }
     render() {
@@ -20,13 +36,14 @@ import React, { Component } from 'react'
             <div>
                 <input value={this.state.value} onChange={this.setvalue}/>
                 <hr></hr>
-                <textarea value={this.state.value} onChange={this.setvalue}></textarea>
+                <textarea value={this.state.valuetextarea} onChange={this.settextareavalue}/>
                 <hr></hr>
                 <select value={this.state.value1} onChange={this.setselectvalue}>
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">three</option>
-                </select>
+                </select><hr></hr>
+                Checkbox<input type="checkbox" checked={this.state.checked} onChange={this.setchecked}/>
             </div>
         )
     }
