449ec3fa6879daa07a0922cd2ae23ef2f04efb31
Mehdi <65984668+MEHDI05061996@users.noreply.github.com>
2020-06-24 21:40:37 +0200

    Update Formik.js

--- component/Formik/Formik.js
+++ component/Formik/Formik.js
@@ -2,29 +2,43 @@ import React, { Component } from 'react';
 import {Formik, Field} from 'formik';
 
 class Formik1 extends Component {
-    onSubmit = (values) => {
-        console.log(values);
-      }
-    
-      form = (props) => {
-        return <form onSubmit={props.handleSubmit}>
-          <label>Name</label><br />
-          <Field name="name" /><br />
-          <label>Email</label><br />
-          <Field name="email" /><br />
-          <button type="submit">Send</button>
-        </form>
-      }
-    render() {
-        return (
-            <div>
-                <Formik 
-          initialValues={{name: "", email: ""}}
+  onSubmit = (values) => {
+    console.log(values);
+  }
+
+  form = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+      <label>Name</label><br />
+      <Field name="name" /><br />
+      <label>Email</label><br />
+      <Field name="email" type="email" /><br />
+      <label>Type</label><br />
+      <Field name="type" component="select">
+        <option value="1">One</option>
+        <option value="2">Two</option>
+      </Field>
+      <br />
+      <label>Active</label><br />
+      <Field name="active" type="checkbox" /><br />
+      <br />
+      <label>Category</label><br />
+      <Field name="category" type="radio" value="1" /> 1<br />
+      <Field name="category" type="radio" value="2" /> 2<br />
+
+      <button type="submit">Send</button>
+    </form>
+  }
+
+  render() {
+    return (
+      <div>
+        <Formik 
+          initialValues={{name: "", email: "", type: "",active: false,category: ""}}
           onSubmit={this.onSubmit}
           render={this.form}
           />
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 export default Formik1;
