e761f35524bbe23dc3aaf0e6781179a4ad49a9c8
Mehdi <65984668+MEHDI05061996@users.noreply.github.com>
2020-06-27 04:06:50 +0200

    Add files via upload

---
+++ Css.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class Css extends Component {
+    state = {
+        className: 'green'
+      }
+    
+      render() {
+        return (
+          <div>
+            <div className={this.state.className}>
+              Test 
+            </div>
+            <button onClick={()=>{this.setState({className: 'red'})}}>
+              change to red
+            </button>
+            <button onClick={()=>{this.setState({className: 'green'})}}>
+              change to green
+            </button>
+          </div>
+        );
+      }
+    }
+
+export default Css;
