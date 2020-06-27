a5244b3bab851253f5ac05079c442136f29cad0f
Mehdi <65984668+MEHDI05061996@users.noreply.github.com>
2020-06-27 04:28:22 +0200

    Add files via upload

--- Css.js
+++ Css.js
@@ -3,13 +3,24 @@ import './App.css';
 
 class Css extends Component {
     state = {
-        className: 'green'
-      }
-    
+        className: 'green',
+
+           active: true,
+       
+      };
+
+      
+
       render() {
+        const style={color:'white', fontWeight: 'bold'};
+        const style1={};
+
+        if(this.state.active){
+          style1.color = 'blue';
+        }
         return (
           <div>
-            <div className={this.state.className}>
+            <div className={this.state.className} style={style}>
               Test 
             </div>
             <button onClick={()=>{this.setState({className: 'red'})}}>
@@ -18,6 +29,14 @@ class Css extends Component {
             <button onClick={()=>{this.setState({className: 'green'})}}>
               change to green
             </button>
+
+            <div className={this.state.active ? 'green': 'red'} style={style1}>
+          Test 
+        </div>
+
+        <button onClick={()=>{this.setState({active: ! this.state.active})}}>
+          change active
+        </button>
           </div>
         );
       }
